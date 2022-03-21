2246bbcffe2cf9fd88422bf7a8a8c82679b17dde
Mareike0308 <mare1702@easv365.dk>
2022-03-21 22:50:41 +0100

    design changes

--- src/GUI/Controller/EsbjergFestivalController.java
+++ src/GUI/Controller/EsbjergFestivalController.java
@@ -1,4 +1,41 @@
 package GUI.Controller;
 
-public class EsbjergFestivalController {
+
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EsbjergFestivalController<JFXButton> implements Initializable {
+
+    @FXML
+    Label EsbjergLbl;
+    @FXML
+    private JFXButton btn;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(20));
+        transition.setNode((Node) btn);
+
+        transition.setToX(-600);
+        transition.setToX(300);
+
+        transition.setAutoReverse(true);
+        transition.setCycleCount(Animation.INDEFINITE);
+        transition.play();
+
+    }
+
+
 }
--- src/GUI/Controller/MainScreenController.java
+++ src/GUI/Controller/MainScreenController.java
@@ -1,13 +1,19 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainScreenController {
+
+    @FXML
+    private Button btnEsbjerg;
 
     public void logIn(ActionEvent actionEvent, Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource("LogInView.fxml"));
@@ -19,5 +25,15 @@ public class MainScreenController {
 
 
     public void logIn(ActionEvent actionEvent) {
+    }
+
+
+    public void openEsbjergFestivalWindow(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("GUI/View/EsbjergFestival.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
