48ca3c1d460a035b07c4ef4e8f050ff545055613
Mareike0308 <mare1702@easv365.dk>
2022-03-24 18:23:32 +0100

    things

--- src/GUI/Controller/NewEventController.java
+++ src/GUI/Controller/NewEventController.java
@@ -1,4 +1,26 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
 public class NewEventController {
+    @FXML
+    private Button btnCancelEvent;
+
+
+    public void cancelAddingEvent(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Discard changes ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
+        //comments
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            Stage stage = (Stage) btnCancelEvent.getScene().getWindow();
+            stage.close();
+        }
+    }
 }
