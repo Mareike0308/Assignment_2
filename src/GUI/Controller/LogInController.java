0559646a7506a1d5f23355a04ca5d96e3e6077a1
Mareike0308 <mare1702@easv365.dk>
2022-04-07 21:51:12 +0200

    added local variable

--- src/GUI/Controller/LogInController.java
+++ src/GUI/Controller/LogInController.java
@@ -2,22 +2,27 @@ package GUI.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LogInController {
+public class LogInController implements Initializable {
 
 
-    public void logIn(ActionEvent actionEvent) {
+    public void logIn(ActionEvent actionEvent) throws IOException {
 
-        /*
-        FXMLLoader fxmlLoader = new FXMLLoader(MainScreenController.class.getResource("LogInView.fxml"));
+
+       /* FXMLLoader fxmlLoader = new FXMLLoader(LogInController.class.getResource("Admin.View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 567, 326);
+        Stage stage = new Stage();
         stage.setTitle("Sign In");
         stage.setScene(scene);
-        stage.show();*/
+        stage.show(); */
     }
 
     public void cancelLogin(ActionEvent actionEvent, Stage stage) throws IOException {
@@ -29,5 +34,10 @@ public class LogInController {
     }
 
     public void cancelLogin(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
