3a69c378375a1c5f23110b7f822617d5af984ede
Mareike0308 <mare1702@easv365.dk>
2022-04-08 21:22:22 +0200

    all events choicebox

--- src/GUI/Controller/CoordinatorViewController.java
+++ src/GUI/Controller/CoordinatorViewController.java
@@ -6,14 +6,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class CoordinatorViewController {
 
     @FXML
     private Button btnEsbjerg;
+
+    @FXML
+    private ChoiceBox eventChoiceBox;
 
     public void logIn(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
@@ -27,15 +32,20 @@ public class CoordinatorViewController {
         stage.show();
     }
 
+    @FXML
+    public void initialize() throws SQLException, IOException {
+        eventChoiceBox.getItems().removeAll(eventChoiceBox.getItems());
+        eventChoiceBox.getItems().addAll("Concerts", "Culture", "Humor", "Party", "Sports");
 
 
 
-    public void openEsbjergFestivalWindow(ActionEvent actionEvent) throws IOException {
+   /* public void openEsbjergFestivalWindow(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getClassLoader().getResource("GUI/View/EsbjergFestival.fxml"));
         Parent root = loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
+    }*/
     }
 }
