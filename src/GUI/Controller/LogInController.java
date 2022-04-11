0d33bda973295444f2967574249642b1e8b5fab0
Mareike0308 <mare1702@easv365.dk>
2022-04-11 16:25:29 +0200

    cancel login

--- src/GUI/Controller/LogInController.java
+++ src/GUI/Controller/LogInController.java
@@ -1,18 +1,26 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogInController implements Initializable {
+
+    @FXML
+    private Button btnCancelLogin;
 
 
     public void logIn(ActionEvent actionEvent) throws IOException {
@@ -39,7 +47,17 @@ public class LogInController implements Initializable {
         stage.show();
     }
 
-    public void cancelLogin(ActionEvent actionEvent) {
+    public void cancelLogin(ActionEvent actionEvent) throws IOException{
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Cancel login?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+        alert.showAndWait();
+        // when closeWindow button is clicked, confirmation window pops up -> user has to confirm if window should be closed
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            Stage stage = (Stage) btnCancelLogin.getScene().getWindow();
+            stage.close();
+        }
+
     }
 
     @Override
